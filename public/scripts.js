4befe79783a2552283de0aa56686e1815d6aa903
James Browning <jabrowni@email.unc.edu>
2023-05-09 17:59:55 -0400

    running out of time

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,101 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+// Function to send a call to the API server
+async function sendAPICall(endpoint, data) {
+  try {
+    const response = await fetch(`https://localhost:5000/app/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('API request failed');
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+// Function to play with an opponent or request a random draw
+async function playGame(opponentChoice) {
+  try {
+    const data = {
+      opponentChoice: opponentChoice,
+    };
+    const result = await sendAPICall('play', data);
+    // Handle the result
+    console.log(result);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+}
+
+// Function to play Rock-Paper-Scissors or Rock-Paper-Scissors-Lizard-Spock
+async function playRPSorRPSLS(gameType, playerChoice) {
+  try {
+    const data = {
+      gameType: gameType,
+      playerChoice: playerChoice,
+    };
+    const result = await sendAPICall('play/rps-or-rpsls', data);
+    // Handle the result
+    console.log(result);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+}
+
+// Function to reset the selections and reset the interface
+function resetInterface() {
+  // Clear the selected choices and reset the interface as needed
+  console.log('Resetting interface...');
+}
+
+// Event listener for play with opponent button click
+document.getElementById('play-opponent-button').addEventListener('click', function () {
+  const opponentChoice = document.querySelector('input[name="opponent-choice"]:checked').value;
+  playGame(opponentChoice);
+});
+
+// Event listener for play without opponent button click
+document.getElementById('play-no-opponent-button').addEventListener('click', function () {
+  playGame(null);
+});
+
+// Event listener for play RPS button click
+document.getElementById('play-rps-button').addEventListener('click', function () {
+  const playerChoice = document.querySelector('input[name="player-choice"]:checked').value;
+  playRPSorRPSLS('rps', playerChoice);
+});
+
+// Event listener for play RPSLS button click
+document.getElementById('play-rpsls-button').addEventListener('click', function () {
+  const playerChoice = document.querySelector('input[name="player-choice"]:checked').value;
+  playRPSorRPSLS('rpsls', playerChoice);
+});
+
+// Event listener for play button click
+document.getElementById('play-button').addEventListener('click', function () {
+  const gameType = document.querySelector('input[name="game-type"]:checked').value;
+  const gameMode = document.querySelector('input[name="game-mode"]:checked').value;
+
+  if (gameMode === 'opponent') {
+    const opponentChoice = document.querySelector('input[name="opponent-choice"]:checked').value;
+    playGame(opponentChoice);
+  } else {
+    playGame(null);
+  }
+});
+
+// Event listener for reset button click
+document.getElementById('reset-button').addEventListener('click', resetInterface);
--- server.js
+++ server.js
@@ -102,3 +102,77 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+import { rps, rpsls } from './controllers/a03-jmsabrwnng/lib/rpsls.js';
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware to set the appropriate headers for all responses
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+// Default API endpoint that returns 404 NOT FOUND for any undefined endpoints
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
+});
+
+// Endpoint /app/ that returns 200 OK
+app.get('/app/', (req, res,next) => {
+  res.status(200).send('200 OK');
+});
+
+// Endpoint /app/rps/
+app.get('/app/rps/', (req, res) => {
+  const result = rps();
+  res.status(200).json(result);
+});
+
+// Endpoint /app/rpsls/
+app.get('/app/rpsls/', (req, res) => {
+  const result = rpsls();
+  res.status(200).json(result);
+});
+
+// Endpoint /app/rps/play/ that accepts shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as request bodies
+app.post('/app/rps/play/', (req, res) => {
+  // Extract the shot from the request body
+  const shot = req.body.shot || req.body;
+
+  // Determine the result
+  const result = rps(shot);
+
+  // Return the response
+  res.status(200).json(result);
+});
+
+// Endpoint /app/rpsls/play/ that accepts shot=(rock|paper|scissors|lizard|Spock) (URLEncoded) or {"shot":"(rock|paper|scissors|lizard|Spock)"} (JSON) as request bodies
+app.post('/app/rpsls/play/', (req, res) => {
+  // Extract the shot from the request body
+  const shot = req.body.shot || req.body;
+
+  // Determine the result
+  const result = rpsls(shot);
+
+  // Return the response
+  res.status(200).json(result);
+});
+
+// Endpoint /app/rps/play/(rock|paper|scissors)/
+app.get('/app/rps/play/:shot', (req, res) => {
+  const result = rps(req.params.shot);
+  res.json(result);
+});
+
+// Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/
+app.post('/app/rpsls/play/:shot', (req, res) => {
+  const result = rpsls(req.params.shot);
+  res.json(result);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
