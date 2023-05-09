6538313aa747b5b2b6a31121e78c89fc989f9c2b
James Browning <jabrowni@email.unc.edu>
2023-05-09 18:41:39 -0400

    hopefully it works

--- server.js
+++ server.js
@@ -102,77 +102,51 @@ process.on('SIGINT', () => {
         }    
     })
 })
-/*
+
 import { rps, rpsls } from './controllers/a03-jmsabrwnng/lib/rpsls.js';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Middleware to set the appropriate headers for all responses
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json');
-  next();
+app.get('/app', (req, res) => {
+    res.status(200).send("200 OK").end();
 });
 
-// Default API endpoint that returns 404 NOT FOUND for any undefined endpoints
-app.use((req, res, next) => {
-  res.status(404).send('404 Not Found');
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps())).end();
 });
 
-// Endpoint /app/ that returns 200 OK
-app.get('/app/', (req, res,next) => {
-  res.status(200).send('200 OK');
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls())).end();
 });
 
-// Endpoint /app/rps/
-app.get('/app/rps/', (req, res) => {
-  const result = rps();
-  res.status(200).json(result);
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
 });
 
-// Endpoint /app/rpsls/
-app.get('/app/rpsls/', (req, res) => {
-  const result = rpsls();
-  res.status(200).json(result);
-});
-
-// Endpoint /app/rps/play/ that accepts shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as request bodies
-app.post('/app/rps/play/', (req, res) => {
-  // Extract the shot from the request body
-  const shot = req.body.shot || req.body;
-
-  // Determine the result
-  const result = rps(shot);
-
-  // Return the response
-  res.status(200).json(result);
-});
-
-// Endpoint /app/rpsls/play/ that accepts shot=(rock|paper|scissors|lizard|Spock) (URLEncoded) or {"shot":"(rock|paper|scissors|lizard|Spock)"} (JSON) as request bodies
-app.post('/app/rpsls/play/', (req, res) => {
-  // Extract the shot from the request body
-  const shot = req.body.shot || req.body;
-
-  // Determine the result
-  const result = rpsls(shot);
-
-  // Return the response
-  res.status(200).json(result);
-});
-
-// Endpoint /app/rps/play/(rock|paper|scissors)/
 app.get('/app/rps/play/:shot', (req, res) => {
-  const result = rps(req.params.shot);
-  res.json(result);
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
 });
 
-// Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/
-app.post('/app/rpsls/play/:shot', (req, res) => {
-  const result = rpsls(req.params.shot);
-  res.json(result);
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-})*/;
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+});
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+});
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+});
+
+app.get('/app/*', (req,res) =>{
+    //Throw a 404 error for other inputs
+    res.status(404).send("404 NOT FOUND");
+});
+
+app.listen(port);
